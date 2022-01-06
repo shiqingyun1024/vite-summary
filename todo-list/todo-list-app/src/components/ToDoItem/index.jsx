66f5fa537930d54c85c09a325c5b05603d8e2e43
814657780 <18037818812@163.com>
2022-01-06 22:40:30 +0800

    vite的学习

--- todo-list/todo-list-app/src/components/ToDoItem/index.jsx
+++ todo-list/todo-list-app/src/components/ToDoItem/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Input } from 'antd'
+import { Input, Button } from 'antd'
 
-const ToDoItem = () =>{
+const ToDoItem = () => {
+    const handleSubmit = () =>{
+        console.log('handleSubmit');
+    }
+    return (
+        <div>
+            <Input />
+            <Button size="large" type="primary" onClick={handleSubmit}>提交</Button>
+        </div>
 
+    )
 }
 
 export default ToDoItem
