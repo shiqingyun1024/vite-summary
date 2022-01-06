d5943917a9f6946f4e3c73f208faa595271d781b
814657780 <18037818812@163.com>
2022-01-06 22:32:56 +0800

    vite的学习

--- todo-list/todo-list-app/src/App.jsx
+++ todo-list/todo-list-app/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import { Button } from 'antd'
+import ToDoItem from './components/ToDoItem'
 
 function App() {
 
   return (
-    <div>
-      <Button type="primary">点击</Button>
-     app
+    <div className="todo-app">
+      <h2 className="todo-title">待办清单</h2>
+      <ToDoItem/>
     </div>
   )
 }
