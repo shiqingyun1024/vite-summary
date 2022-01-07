a73afcfa8be55673a6d878ddd4ffe61920320bf1
814657780 <18037818812@163.com>
2022-01-07 22:01:37 +0800

    vite的学习

--- todo-list/todo-list-app/src/components/ToDoItem/index.jsx
+++ todo-list/todo-list-app/src/components/ToDoItem/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input, Button } from 'antd'
+import { STATUS } from '../../config/status'
+// import { useState } from 'react'
 
 const todoItem = {
     id:123,
@@ -7,12 +9,17 @@ const todoItem = {
     status:0
 }
 const ToDoItem = () => {
+    const { todoItem,setTodoItem } = useState({
+        id:Math.random(),
+        content:'',
+        status:STATUS.IS_CREATE
+    })
     const handleSubmit = () =>{
         console.log('handleSubmit');
     }
     return (
         <div className="todo-item-input">
-            <Input />
+            <Input value={todoItem.content}/>
             <Button size="large" type="primary" onClick={handleSubmit}>提交</Button>
         </div>
 
