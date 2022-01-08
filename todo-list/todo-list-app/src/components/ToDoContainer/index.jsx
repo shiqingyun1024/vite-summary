b60d349c4604fefe518fbb94095911babdd723a9
814657780 <18037818812@163.com>
2022-01-08 20:50:08 +0800

    vite的学习

--- todo-list/todo-list-app/src/components/ToDoContainer/index.jsx
+++ todo-list/todo-list-app/src/components/ToDoContainer/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { List } from 'antd'
 
-const ToDoContainer = () =>{
+const ToDoContainer = (props) => {
+    const {todos=[]} = props
     return (
-      <div className="todo-container">
-
-      </div>
+        <div className="todo-container">
+            <List
+                dataSource={todos}
+                renderItem={
+                    todo=>(
+                        <List.Item>
+                            {todo.content}
+                        </List.Item>
+                    )
+                }
+            />
+        </div>
     )
 }
 export default ToDoContainer
