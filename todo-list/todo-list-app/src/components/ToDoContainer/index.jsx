a76476b87f9b7e36f30b4796bc338f09a4b72cf4
814657780 <18037818812@163.com>
2022-01-08 21:10:24 +0800

    vite的学习

--- todo-list/todo-list-app/src/components/ToDoContainer/index.jsx
+++ todo-list/todo-list-app/src/components/ToDoContainer/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { List } from 'antd'
+import { CheckCircleTwoTone,CloseCircleTwoTone } from '@ant-design/icons'
 
 const ToDoContainer = (props) => {
     const {todos=[]} = props
@@ -9,8 +10,12 @@ const ToDoContainer = (props) => {
                 dataSource={todos}
                 renderItem={
                     todo=>(
-                        <List.Item>
+                        <List.Item className="todo-container-list">
                             {todo.content}
+                            <div className="todo-item-operate">
+                                <CheckCircleTwoTone/>
+                                <CloseCircleTwoTone/>
+                            </div>
                         </List.Item>
                     )
                 }
