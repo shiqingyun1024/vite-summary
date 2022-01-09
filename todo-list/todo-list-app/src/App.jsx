d5c75110eb1ad155d0f540c673cb28ce102a841a
814657780 <18037818812@163.com>
2022-01-09 21:59:53 +0800

    vite的学习

--- todo-list/todo-list-app/src/App.jsx
+++ todo-list/todo-list-app/src/App.jsx
@@ -5,6 +5,7 @@ import ToDoContainer from './components/ToDoContainer'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [ filterStatus, setFilterStatus] = useState('');
   const handleSubmit = (todoItem) => {
     console.log('App', todoItem);
     setTodos([
@@ -23,7 +24,7 @@ function App() {
     <div className="todo-app">
       <h2 className="todo-title">待办清单</h2>
       <ToDoItem onSubmit={handleSubmit} />
-      <ToDoFilter/>
+      <ToDoFilter filterStatus={filterStatus}/>
       <ToDoContainer todos={todos} onOperate={handleOperate}/>
     </div>
   )
