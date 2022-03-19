9f82a503ed3dc07c8cdda274ac50b1c571c6683c
814657780 <18037818812@163.com>
2022-03-19 22:18:21 +0800

    vite+vue3+elementplus

---
+++ vue3.0+vite/vue3addvite/src/views/Login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="120px"
+      class="demo-ruleForm"
+      :size="formSize"
+    >
+      <el-form-item label="Activity name" prop="name">
+        <el-input v-model="ruleForm.name" />
+      </el-form-item>
+      <el-form-item label="Activity name" prop="name">
+        <el-input v-model="ruleForm.name" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+export default {};
+</script>
+
+<style>
+</style>
