fb3e47bce64f770d3a63c984b272214a34e97310
814657780 <18037818812@163.com>
2022-03-19 22:56:58 +0800

    vite+vue3+elementplus

--- vue3.0+vite/vue3addvite/src/views/Login/index.vue
+++ vue3.0+vite/vue3addvite/src/views/Login/index.vue
@@ -2,24 +2,38 @@
   <div class="login">
     <el-form
       ref="ruleFormRef"
-      :model="ruleForm"
+      :model="temp"
       :rules="rules"
       label-width="120px"
       class="demo-ruleForm"
       :size="formSize"
     >
-      <el-form-item label="Activity name" prop="name">
-        <el-input v-model="ruleForm.name" />
+      <el-form-item label="用户名" prop="user">
+        <el-input v-model="temp.user" />
       </el-form-item>
-      <el-form-item label="Activity name" prop="name">
-        <el-input v-model="ruleForm.name" />
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="temp.password" />
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
-export default {};
+import { reactive, ref } from 'vue'
+const temp = reactive({
+  user: '',
+  password: ''
+})
+const rules = reactive({
+  name: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 18, message: 'Length should be 3 to 18', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 3, max: 18, message: 'Length should be 3 to 18', trigger: 'blur' },
+  ]
+})
 </script>
 
 <style>
