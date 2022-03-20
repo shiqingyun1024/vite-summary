8b70ff513bef6388c3ad3774e0f0fb8a5455d2ff
814657780 <18037818812@163.com>
2022-03-20 22:16:26 +0800

    vite+vue3+elementPlus+ts

--- vue3.0+vite/vue3addvite/vite.config.ts
+++ vue3.0+vite/vue3addvite/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
+// const resolve = (dir: string) => path.join(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find:'@',
+        replacement:resolve(__dirname,'src')
+      },
+    ]
+  },
   plugins: [vue()]
 })
+function __dirname(__dirname: any, arg1: string): string {
+  throw new Error('Function not implemented.')
+}
+
